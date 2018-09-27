becf76072c4a4d8c018a0de0cd3c5f41f2f342ae
tomney <gregory.tomney@gmail.com>
2018-09-26 21:48:00 -0400

    import HttpClientModule to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { HeadToHeadComponent } from './headtohead.component';
 import { HeadToHeadService } from './headtohead.service';
+import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from '../../node_modules/@angular/common/http';
 
 
@@ -11,7 +12,8 @@ import { HttpClient } from '../../node_modules/@angular/common/http';
     HeadToHeadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    HttpClientModule
   ],
   providers: [HeadToHeadService, HttpClient],
   bootstrap: [HeadToHeadComponent]
